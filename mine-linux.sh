d5deb1ba53b02d8e42ebeb490425212693ca151b
Crypto-Robin-Hood <cryptorobinhood@gmx.com>
2020-12-05 16:38:26 +0200

    Add Sh script for miner

---
+++ mine-linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while :
+do
+    ~/**/**/bitcoinrand-cli generate 1
+	echo "Press [CTRL+C] to stop.."
+	sleep 1
+done
---
+++ mine.bat
@@ -0,0 +1,1 @@
+@echo off set SCRIPT_PATH=%cd% cd %SCRIPT_PATH% echo Press [CTRL+C] to stop mining. :begin bitcoinrand-cli.exe generate 1 goto begin
